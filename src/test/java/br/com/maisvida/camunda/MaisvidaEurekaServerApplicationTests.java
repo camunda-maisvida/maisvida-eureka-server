fe8747acfed756c961f6fcbaa5206e854f4893a6
Rodolfo Cruz <rodolfocruz.ti@gmail.com>
2019-11-10 11:17:21 -0300

    Initial commits

---
+++ src/test/java/br/com/maisvida/camunda/MaisvidaEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maisvida.camunda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaisvidaEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
